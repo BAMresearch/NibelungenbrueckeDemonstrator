e7023d1d859bb9f5fcd350f2f91b5e8bcb5cf761
msoenmez <msoenmez@VDI0070.zit.bam.de>
2024-02-21 16:27:33 +0100

    Modifications of sensor translator module

--- nibelungenbruecke/scripts/utilities/API_sensor_retrieval.py
+++ nibelungenbruecke/scripts/utilities/API_sensor_retrieval.py
@@ -14,21 +14,24 @@ import h5py
 
 
 class API_Request:
+    """
+    A class to handle API requests.
+
+    Attributes:
+        url: The API endpoint URL.
+        headers: Request headers.
+        params: URL parameters.
+        body: Request body parameters.
+    """
     
     def __init__(self):
-        
-        # /samples API-Endpunkt 
         self.url = "https://func-70021-nibelungen-export.azurewebsites.net/samples"
- 
         self.headers = {
             "Content-Type": "application/json"
             }
- 
-        # url parameter 
         self.params = {
             "code": "nv8QrKftsTHj93hPM4-BiaJJYbWU7blfUGz89KdkuEbpAzFuHX1Rmg==" # der Code aus den über Keeper mitgetielten Zugangdaten 
         }  
-
         self.body = {
             "startTime": "2023-08-11T08:00:00Z",
             "endTime": "2023-08-11T09:00:00Z",
@@ -38,7 +41,6 @@ class API_Request:
              'E_plus_413TU_HSS-m-_Avg1',
              ]
             }
-        
         
         """
         self.body = {
@@ -141,27 +143,20 @@ class API_Request:
             
             plt.show()
        
-    def API(self): 
-        
-        # Senden der POST-Anfrage
+    def fetch_data(self):
+        """
+        Fetch data from the API and store it in a DataFrame.
+        """
+
         response = requests.post(self.url, headers=self.headers, params=self.params, json=self.body)
-         
-        # Überprüfen, ob die Anfrage erfolgreich war
+
         if response.status_code != 200:
             raise ValueError(f"Anfrage fehlgeschlagen mit Statuscode {response.status_code}: {response.text}")
-         
-        # Daten aus der Antwort extrahieren 
         data = response.json()
-         
-        # in einen DataFrame laden
         self.df = pd.DataFrame(data["rows"], columns=[col["ColumnName"] for col in data["columns"]])
-         
-        # Optional: Timestamp-Spalte konvertieren und als Index setzen
         #self.df["Timestamp"] = pd.to_datetime(self.df["Timestamp"], format="ISO8601")
         self.df["Timestamp"] = pd.to_datetime(self.df["Timestamp"])
         self.df = self.df.set_index("Timestamp")
-         
-        # Aufgrund unterschiedlicher Abtastraten der Sensoren enthält DataFrame NaN-Werte
         # print(self.df)
         return pd.DataFrame(self.df[self.df.columns], index=pd.to_datetime(self.df.index))
 
@@ -176,10 +171,9 @@ import datetime
 import h5py
 from datetime import datetime, timedelta
 
-class saveAPI:
+class MetadataSaver:
     
-    def __init__(self, path_meta, df, path_df):
-
+    def __init__(self, path, df):
         self.data = {
             "df": {
                 "columns": [],
@@ -192,164 +186,137 @@ class saveAPI:
                 "Humidity": []
             }
         }
-        
-        self.path_meta = path_meta
-        self.path_df = path_df
+        self.path_meta = path["meta_output_path"]
+        self.path_df = path["df_output_path"]
         self.df = df
          
-    def save(self):
+    def saving_metadata(self):
 
         # Origin: "49.630742, 8.378049"
         
         for i in range(len(self.df.columns)):
             if self.df.columns[i] == 'E_plus_413TU_HS--o-_Avg1':
-                
                 column_name = self.df.columns[i]
-                    
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [44.4925, 4.74, 0.0],
-                "height": 104.105                       
+                "coordinate": [41.42, 0.0, 0.0],
+                "height": 107.438                        
             })
 
             elif self.df.columns[i] == 'E_plus_413TU_HSS-m-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [44.4925, 3.69, 0.0],
-                "height": 105                       
+                "coordinate": [41.42, -3.69, 0.0],
+                "height": 103.748                      
             })
 
             elif self.df.columns[i] == 'E_plus_413TU_HS--u-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [44.4925, 2.29, 0.0],
-                "height": 106                         
+                "coordinate": [41.42, -4.74, 0.0],
+                "height": 102.698                        
             })
                 
             elif self.df.columns[i] == 'E_plus_423NU_HSN-o-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [45.3425, 5.54, 0.0],
-                "height": 107                          
+                "coordinate": [42.22, -3.33, 0.0],
+                "height": 102.698                          
             })
             
             elif self.df.columns[i] == 'E_plus_040TU_HS--o-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [1, 4.74, 0.0],
-                "height": 108                         
+                "coordinate": [4, 0.0, 0.0],
+                "height": 107.438                         
             })
 
             elif self.df.columns[i] == 'E_plus_040TU_HSN-m-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [1, 2.37, 0.005],
-                "height": 109                         
+                "coordinate": [4, -2.37, 0.005],
+                "height": 105.068                        
             })
 
             elif self.df.columns[i] == 'E_plus_040TU_HSS-m-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [0.92, 2.37, 0.005],
-                "height": 104.105                       
+                "coordinate": [4, -2.37, 0.005],
+                "height": 105.068                       
             })
 
             elif self.df.columns[i] == 'E_plus_040TU_HS--u-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [1, 0.0, 0.355],
-                "height": 104.105                       
+                "coordinate": [4, -4.74, 0.355],
+                "height": 102.698                      
             })
             
             elif self.df.columns[i] == 'E_plus_413TI_HSS-m-_Avg':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [1, 0.0, 0.0],
-                "height": 104.105                       
+                "coordinate": [41.42, -3.33, 0.0],
+                "height": 102.698                       
             })
 
             elif self.df.columns[i] == 'E_plus_040TI_HSS-u-_Avg':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [1, 0.0, 0.0],
+                "coordinate": [4, -4.74, 0.0],
                 "height": 104.105                       
             })
 
             elif self.df.columns[i] == 'E_plus_423NUT_HSN-o-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [45.3425, 4.34, 0.0],
-                "height": 104.105                       
+                "coordinate": [42.22, 0.0, 0.0],
+                "height": 107.438                       
             })
 
             elif self.df.columns[i] == 'E_plus_467NUT_HSN-o_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [49.8925, 4.34, 0.0],
-                "height": 104.105                       
+                "coordinate": [46.82, 0.0, 0.0],
+                "height": 107.438                       
             })
 
             elif self.df.columns[i] == 'F_plus_000TA_KaS-o-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Temp"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
@@ -359,27 +326,23 @@ class saveAPI:
             })
 
             elif self.df.columns[i] == 'E_plus_445LVU_HS--o-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Move"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [47.5925, 4.74, 0.0],
-                "height": 104.105                       
+                "coordinate": [44.52, 0.0, 0.0],
+                "height": 107.438                       
             })
 
             elif self.df.columns[i] == 'E_plus_445LVU_HS--u-_Avg1':
-
                 column_name = self.df.columns[i]
-                
                 self.data["meta"]["Move"].append({
                 "name": column_name,
                 "unit": "\u00b0C",
                 "sample_rate": 0.0016666666666666668,   
-                "coordinate": [47.5925, 0.0, 0.0],
-                "height": 104.105                       
+                "coordinate": [44.52, -4.74, 0.0],
+                "height": 102.698                       
             })
 
         with open(self.path_meta, "w") as json_file:
@@ -393,7 +356,6 @@ class saveAPI:
             
         # Saving dataframe of the request as json
         #self.df.to_json(self.path_df)
-        
         
         #print(self.data)
         return self.path_meta, self.path_df
@@ -411,9 +373,10 @@ from pyproj import Proj, transform
 
 class Translator:
     
-    def __init__(self, meta_path: Union[str, bytes, PathLike], **kwargs):
+    def __init__(self, path, **kwargs):
         self.columns = ["Temp", "Move", "Humidity"]
-        self.meta_path = meta_path
+        self.path = path
+        self.meta_path = self.path["meta_output_path"]
         self.kwargs = kwargs
     
     def _default_parameters(self):
@@ -421,12 +384,11 @@ class Translator:
             "sensors": []
         }
 
-    def translator_to_sensor(self, meta_output_path):
+    def translator_to_sensor(self):
+        self.MKP_meta_output_path = self.path["MKP_meta_output_path"]
         default_parameters_data = self._default_parameters()
-
         with open(self.meta_path, 'r') as f:
-            self.j = json.load(f)
-            
+            self.j = json.load(f)    
         self.meta = self.j["meta"]
 
         for key in self.columns:
@@ -452,35 +414,10 @@ class Translator:
                 
                     default_parameters_data["sensors"].append(sensor_data)
                 
-        with open(meta_output_path, "w") as f:
+        with open(self.MKP_meta_output_path, "w") as f:
             json.dump(default_parameters_data, f, indent=4)
 
-        return meta_output_path
-    
-    def save_displacement_values(self, path, disp_values):
-        """
-        Save displacement values corresponding to each sensor ID in the MKP_meta_output_path JSON file.
-
-        Parameters:
-            disp_values: Displacement values.
-        """
-        self.path = path
-        with open(self.path["MKP_meta_output_path"], 'r') as f:
-            metadata = json.load(f)
-
-        virtual_sensors = []
-        for sensor in metadata["sensors"]:
-            sensor_id = sensor["id"]
-            position = sensor["where"]
-            displacement_value = disp_values.sensors.get(sensor_id, None)
-            if displacement_value is not None:
-                displacement_value_list = displacement_value.data[0].tolist()  # Convert ndarray to list
-                virtual_sensors.append({"id": sensor_id, "displacement": displacement_value_list})
-
-        metadata["virtual_sensors"] = virtual_sensors
-
-        with open(self.path["MKP_meta_output_path"], 'w') as f:
-            json.dump(metadata, f, indent=4)
+        return self.MKP_meta_output_path
 
     @staticmethod
     def cartesian_to_geodesic(cartesian, origin=[49.630742, 8.378049]):
@@ -525,9 +462,9 @@ class Translator:
 
         return f"{utm_zone_number} {utm_zone_letter} E{utm_easting_str} N{utm_northing_str}"
     
-    def save_to_MKP(self, df, displacement_meta_path, output_path):
-
-        # Create a dictionary representing the JSON structure
+    def save_to_MKP(self, df):
+        self.MKP_input_path = self.path["MKP_meta_output_path"]
+        self.translated_data_path = self.path["MKP_translated_output_path"]
         json_data = {
             "df": {
                 "columns": df.columns.tolist(),
@@ -537,8 +474,7 @@ class Translator:
             "meta": {}
         }
 
-        # Load displacement metadata
-        with open(displacement_meta_path["MKP_meta_output_path"], "r") as file:
+        with open(self.MKP_input_path, "r") as file:
             displacement_data = json.load(file)
 
         for column in df.columns:
@@ -553,38 +489,28 @@ class Translator:
                 "height": "" 
             }
 
-        with open(output_path, "w") as json_file:
+        with open(self.translated_data_path, "w") as json_file:
             json.dump(json_data, json_file, indent=4)
 
-    def save_VS(self, path_01, path_02, displacement_values):
-        # Load metadata from path_01
-        with open(path_01["MKP_meta_output_path"], 'r') as f:
+    def save_virtual_sensor(self, displacement_values):
+        with open(self.MKP_input_path, 'r') as f:
             metadata = json.load(f)
 
-        # Load existing data from path_02 or create an empty dictionary
-        try:
-            with open(path_02, 'r') as f:
-                MKP_data = json.load(f)
-        except FileNotFoundError:
-            MKP_data = {}
-
-         # Check if "virtual_sensors" already exists, if not, create it
+        with open(self.translated_data_path, 'r') as f:
+            MKP_data = json.load(f)
+        
         if "virtual_sensors" not in MKP_data:
             MKP_data["virtual_sensors"] = {}
-
-        #virtual_sensors = MKP_data["virtual_sensors"]
 
         for sensor in metadata["sensors"]:
             sensor_id = sensor["id"]
             position = sensor["where"]
-            if displacement_values is not None:
-                displacement_value_list = displacement_values
+            displacement_value = displacement_values.sensors.get(sensor_id, None)
+            if displacement_value is not None:
+                displacement_value_list = displacement_value.data[0].tolist()
                 if sensor_id not in MKP_data["virtual_sensors"]:
                     MKP_data["virtual_sensors"][sensor_id] = {"displacements": []}
-                # Append the new displacement values list to the existing "displacements" array
-                MKP_data["virtual_sensors"][sensor_id]["displacements"].extend(displacement_value_list)
+                MKP_data["virtual_sensors"][sensor_id]["displacements"].append(displacement_value_list)
 
-
-        with open(path_02, 'w') as f:
+        with open(self.translated_data_path, 'w') as f:
             json.dump(MKP_data, f, indent=4)
-
